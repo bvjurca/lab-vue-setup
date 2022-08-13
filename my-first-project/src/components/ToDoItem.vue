85d1e6f413aec8a3a8fe5322cbc3b1e07783110c
bogdinamitawhoami <bogdifromsalem@yahoo.com>
2022-08-13 21:47:46 +0200

    Iteration 2 done

--- my-first-project/src/components/ToDoItem.vue
+++ my-first-project/src/components/ToDoItem.vue
@@ -1,8 +1,13 @@
 <template>
-   <h1>Este es el comopnente balbala</h1>
+   <h1>Message: {{ message }} </h1>
 </template>
 <script>
 export default {
   name: 'ToDoItem',
+  data() {
+    return {
+      message: 'This is the component in a variable',
+    };
+  },
 };
 </script>
