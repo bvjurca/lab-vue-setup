02a06f7a2a2b4a8436c77ccbeec9962bbd5b18cf
bogdinamitawhoami <bogdifromsalem@yahoo.com>
2022-08-13 14:36:07 +0200

    Iteration 1 done

---
+++ my-first-project/src/components/ToDoItem.vue
@@ -0,0 +1,8 @@
+<template>
+   <h1>Este es el comopnente balbala</h1>
+</template>
+<script>
+export default {
+  name: 'ToDoItem',
+};
+</script>
---
+++ my-first-project/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <ToDoItem></ToDoItem>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import ToDoItem from '@/components/ToDoItem.vue';
+
+export default {
+  name: 'ToDoItem',
+  components: {
+    ToDoItem,
+  },
+};
+</script>
